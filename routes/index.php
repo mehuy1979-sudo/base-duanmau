8dc2665f0d9043345ee572f1b30012531441411e
CongNguyenChi <chesecong@gmail.com>
2026-08-08 11:09:06 +0700

    Admin

--- routes/index.php
+++ routes/index.php
@@ -1,17 +1,11 @@
-<!-- <?php
+<?php
 
 $action = $_GET['action'] ?? '/';
 
 match ($action) {
     '/'         => (new HomeController)->index(),
      '/product'         => (new HomeController)->index(),
-}; -->
-
-<?php
-
-$action = $_GET['action'] ?? '/';
-
-match ($action) {
-    '/'        => (new HomeController)->index(),
     '/product' => (new ProductController)->index(),
-};
+     }; 
+
+
